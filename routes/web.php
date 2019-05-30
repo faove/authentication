3e014b9532e3e31e324b67fd4be5752307795c11
Francisco Alvarez <faove@hotmail.com>
2019-05-30 15:17:18 -0400

    Modifique la rutas archivo web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,25 @@ Route::get('/', function () {
 });
 //->middleware('auth.basic');
 
-Auth::routes();
+//Auth::routes();
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+//Registration Routes...
+if ($options['register'] ?? true) {
+   Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+   Route::post('register', 'Auth\RegisterController@register');
+}
+
+// Password Reset Routes...
+        if ($options['reset'] ?? true) {
+            Route::resetPassword();
+        }
+
+        // Email Verification Routes...
+        if ($options['verify'] ?? false) {
+            Route::emailVerification();
+        }
 
 Route::get('/home', 'HomeController@index')->name('home');
